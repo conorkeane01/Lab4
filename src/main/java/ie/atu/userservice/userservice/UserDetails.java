7d1a06f25e194a5851a820e3512dcdd3bd0aa038
conorkeane <g00391846@atu.ie>
2023-10-11 23:18:58 +0100

    Created UserDetails Class and added data, using lombok

--- src/main/java/ie/atu/userservice/userservice/UserDetails.java
+++ src/main/java/ie/atu/userservice/userservice/UserDetails.java
@@ -1,4 +1,13 @@
 package ie.atu.userservice.userservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDetails {
+    private String name;
+    private String email;
 }
