753f7b6701b87f3b9742addebb3f44454976a3f4
conorkeane <g00391846@atu.ie>
2023-10-11 22:04:13 +0100

    Created UserDetails Class

--- src/main/java/ie/atu/userservice/userservice/RegistrationServiceClient.java
+++ src/main/java/ie/atu/userservice/userservice/RegistrationServiceClient.java
@@ -1,4 +1,11 @@
 package ie.atu.userservice.userservice;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = confirmation-service, url = "http:localhost:8080")
 public interface RegistrationServiceClient {
+    @PostMapping("/confirm")
+    String someDetails(@RequestBody UserDetails userDetails)
 }
